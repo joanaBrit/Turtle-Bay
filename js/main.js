7d8b422ae283943a5b35b9c91ef9209536ae76d5
joana <joanabrito216@gmail.com>
2023-08-15 10:40:03 +0100

    Added updated work

--- js/main.js
+++ js/main.js
@@ -1,41 +1,289 @@
 // ? GRID CREATION
 
 //Board Elements
-
 const grid = document.querySelector('#grid')
-console.log(grid)
-//Board Variables
 
+//how to put the grid as an array to use
+// grid = [[null, null, null, null, null, null, null, null, null, null, null], \
+// ['shark', null, 'shark', null, 'shark', 'shark', null, null, 'shark', null, 'shark'], \
+// [null, 'shark', 'shark', null, null, 'shark', 'shark', null, 'shark', 'shark', null], \
+// [null, null, null, null, null, null, null, null, null, null, null], \
+// ['seagull', null, null, 'seagull', null, null, 'segull', null, null, 'seagull', null], \
+// [null, null, null, null, null, null, null, null, null, null, null], \
+// [null, null, null, null, null, null, null, null, null, null, null], \
+// [null, null, null, null, null, null, null, null, null, null, null], \
+// [null, null, 'crab', null, null, 'crab', null, null, 'crab', null, null], \
+// [null, null, null, null, null, null, null, null, null, null, null], \
+// [null, null, null, null, null, null, null, null, null, null, null], \
+// [null, null, null, null, null, 'turtle', null, null, null, null, null]]
+
+//Board Variables
 const width = 11
 const cellCount = width * width
-// cells
+const cells = []
+
+//Turtle variables
+let startTurtle = 115
+let currentTurtlePosition = 115
+
+//Animals position
+
+//crab variables
+let startCrabPosition = [89, 92, 95, 98]
+let currentCrabPosition = [89, 92, 95, 98]
+
+//seagull variables
+let startSeagullPosition = [55, 58, 60, 62, 65]
+let currentSegullPosition = [55, 58, 60, 62, 65]
+
+//shark variables
+
+//Left
+let startSharkLPosition = [33, 34, 36, 37, 39, 40, 42, 43]
+let currentSharkLPosition = [33, 34, 36, 37, 39, 40, 42, 43]
+
+//Right
+let startSharkRPosition = [22, 24, 26, 27, 30, 32]
+let currentSharkRPosition = [22, 24, 26, 27, 30, 32]
 
 
 //Generate the cells
+function generateGrid() {
+  grid.innerHTML = ''
 
-for(let i = 0; i < cellCount; i++) {
-  const cell = document.createElement('div')
-  cell.classList.add('cell')
-  grid.append(cell)
-  cell.style.width = `${100 / width}%`
-  cell.style.height = `${100 / width}%`
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+    cell.style.width = `${100 / width}%`
+    cell.style.height = `${100 / width}%`
+    //!remove this later
+    cell.innerHTML = i
+    cell.dataset.index = i
+    grid.append(cell)
+    cells.push(cell)
+
+    if (i <= 10) {
+      cell.classList.add('firstrow')
+    } else if (i <= 21) {
+      cell.classList.add('secondrow')
+    } else if (i <= 32) {
+      cell.classList.add('thirdrow')
+    } else if (i <= 43) {
+      cell.classList.add('fourthrow')
+    } else if (i <= 54) {
+      cell.classList.add('fifthrow')
+    } else if (i <= 65) {
+      cell.classList.add('sixthrow')
+    } else if (i <= 76) {
+      cell.classList.add('seventhrow')
+    } else if (i <= 87) {
+      cell.classList.add('eighthrow')
+    } else if (i <= 98) {
+      cell.classList.add('ninthrow')
+    } else if (i <= 109) {
+      cell.classList.add('tenthrow')
+    } else {
+      cell.classList.add('eleventhrow')
+    }
+  }
+
+  addTurtle(115)
+
+  startCrabPosition.forEach(index => addCrab(index))
+
+  startSeagullPosition.forEach(index => addSeagull(index))
+
+  startSharkLPosition.forEach(index => addSharkL(index))
+
+  startSharkRPosition.forEach(index => addSharkR(index))
+
 }
 
-// Function for generating the cells
-// create a div 
-//  give a class
-// set the width
-// set the height
-// add index to the cell
-// add data to index 0,1,2,3...
-// add the new cell to the grid 
-// add the new cell created to our cells [], so we have access
 
-//add turtle to start position
+generateGrid()
 
 
-//animals obstacles 
 
-// add crabs
-// add  seagles
-// add sharks
+//?Game Functionality
+
+//*Elements
+
+
+
+
+// variables
+let score = 0
+let interval
+let lives = 3
+//*Executions
+
+//? Turtle
+
+//!add turtle 
+function addTurtle(position) {
+  cells[position].classList.add('turtle')
+}
+
+
+//remove turtle
+function removeTurtle() {
+  cells[currentTurtlePosition].classList.remove('turtle')
+}
+
+
+function moveTurtle(event) {
+
+  const key = event.keyCode
+  const up = 38
+  const down = 40
+  const right = 39
+  const left = 37
+
+  //remove Turtle
+  removeTurtle()
+
+  if (key === up && currentTurtlePosition >= width) {
+    currentTurtlePosition -= width
+
+  } else if (key === down && cellCount - 1 >= currentTurtlePosition + width) {
+    currentTurtlePosition += width
+
+  } else if (key === right && currentTurtlePosition % width !== width - 1) {
+    currentTurtlePosition++
+
+  } else if (key === left && currentTurtlePosition % width !== 0) {
+    currentTurtlePosition--
+
+  } else {
+    console.log('Invalid')
+  }
+  //add turtle to new position
+  addTurtle(currentTurtlePosition)
+
+}
+
+//? Animals
+
+//!add crab
+function addCrab(position) {
+  cells[position].classList.add('crab')
+}
+
+//remove crab
+function removeCrab(position) {
+  cells[position].classList.remove('crab')
+}
+
+//function move crab left to the right
+function moveCrab() {
+  console.log('Moving crab')
+  currentCrabPosition.forEach((position, i) => {
+    // remove crab at this position
+    removeCrab(position)
+    // calculate new crab positions, moving just in the same row
+    let newPosition = position === 87 ? 77 : position + 1
+    currentCrabPosition[i] = newPosition
+    // add new crabs at the updated currentCrabPosition
+    addCrab(newPosition)
+  })
+
+}
+// setInterval(moveCrab, 1000)
+// set speed
+// clearInterval(interval)
+
+
+//!add seagull
+function addSeagull(position) {
+  cells[position].classList.add('seagull')
+}
+
+//remove seagull
+function removeSeagull(position) {
+  cells[position].classList.remove('seagull')
+}
+
+// function move seagull right to the left
+function moveSeagull() {
+  console.log('Move seagull')
+  currentSegullPosition.forEach((position, i) => {
+    //remove seagull at this position
+    removeSeagull(position)
+    //calculate new seagull positions, moving in the same row
+    let newPosition = position === 44 ? 54 : position - 1
+    currentSegullPosition[i] = newPosition
+    // add seagulls to the new position
+    addSeagull(newPosition)
+  }
+
+  )
+}
+// setInterval(moveSeagull, 1000)
+//set speed
+// clearInterval
+
+//!add shark left to right
+function addSharkL(position) {
+  cells[position].classList.add('sharkleft')
+}
+
+//remove shark left
+function removeSharkL(position) {
+  cells[position].classList.remove('sharkleft')
+}
+
+//function move sharks left to the right
+function moveSharkL() {
+  currentSharkLPosition.forEach((position, i) => {
+    //remove shark at this position
+    removeSharkL(position)
+    //calculate new shark positions, moving in the same row
+    let newPosition = position === 32 ? 22 : position + 1
+    currentSharkLPosition[i] = newPosition
+    // add sharks to the new position
+    addSharkL(newPosition)
+  })
+}
+
+// setInterval(moveSharkL, 1000)
+// set speed
+// clearInterval()
+
+//!add shark right to left
+function addSharkR(position) {
+  cells[position].classList.add('sharkright')
+}
+//remove shark left
+function removeSharkR(position) {
+  cells[position].classList.remove('sharkright')
+}
+
+//function move sharks right to the left
+function moveSharkR() {
+  currentSharkRPosition.forEach((position, i) => {
+    //remove shark at this position
+    removeSharkR(position)
+    //calculate new shark positions, moving in the up row and down
+    let newPosition = position === 11 ? 10 : position - 1 && position === 0 ? 21 : position - 1
+
+    currentSharkRPosition[i] = newPosition
+    // add sharks to the new position
+    addSharkR(newPosition)
+  })
+}
+
+// setInterval(moveSharkR, 1000)
+//set speed
+// clearInterval
+
+//*Events
+// keyup triggers once
+// keydown triggers infinitely if key is held down
+
+//Keypress event to move the turtle
+document.addEventListener('keyup', moveTurtle)
+
+
+
+//! chalange
+//move sharks !straight line
